50c518e149c9882b8995c782a7d23d38cf68b18f
Bhashkar Sharma <bhashkarsharma@gmail.com>
2022-11-12 12:25:15 +0100

    Add mastodon link to footer.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,13 @@
 import { tw } from 'brise';
 import React from 'react';
-import { FaEnvelope, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { FaEnvelope, FaGithub, FaInstagram, FaMastodon, FaTwitter } from 'react-icons/fa';
 import { SITE } from '~/constants/global';
 
 const LINKS = {
     github: 'https://github.com/bhashkarsharma',
     instagram: 'https://instagram.com/play.pixels',
     twitter: 'https://twitter.com/bhashkarsharma',
+    mastodon: 'https://fosstodon.org/@bsharma',
     email: 'mailto:info@bhashkar.me',
 };
 
@@ -14,7 +15,7 @@ const FooterWrapper = tw.footer`
   p-8
   pb-2
   footer
-  bg-primary
+  bg-secondary
   footer-center
 `;
 
@@ -31,9 +32,12 @@ function FooterLink({ href, children }: React.AnchorHTMLAttributes<HTMLAnchorEle
 export default function Footer() {
     return (
         <FooterWrapper>
-            <div className="grid grid-cols-4 gap-6">
+            <div className="grid grid-cols-5 gap-6">
                 <FooterLink href={LINKS.github}>
                     <FaGithub size={iconSize} />
+                </FooterLink>
+                <FooterLink href={LINKS.mastodon}>
+                    <FaMastodon size={iconSize} />
                 </FooterLink>
                 <FooterLink href={LINKS.twitter}>
                     <FaTwitter size={iconSize} />
