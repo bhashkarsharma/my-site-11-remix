31ec7a06a1568f2bc2ddfcf149ce5176ade7c26c
Bhashkar Sharma <bhashkarsharma@gmail.com>
2022-11-12 12:55:16 +0100

    Updated about page.

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -11,10 +11,23 @@ export const meta: MetaFunction = () => {
 
 export default function About() {
     return (
-        <div className="content-wrapper">
+        <div className="prose-wrapper">
             <PageTitle>Hi there! 👋</PageTitle>
             <p>Nice to meet you. I am Bhashkar.</p>
-            <p>🚧 I am currently building my site. Please visit again. 🚧</p>
+            <p>I am from 🇮🇳, currently living in 🇩🇪. Ich kann ein bisschen Deutsch sprechen.</p>
+            <p>
+                I spend way too much time in front of screens, either focusing on writing code or
+                text; or relaxing by reading or watching anything that catches my fancy.
+            </p>
+            <p>
+                I have been professionally writing code for almost 13 years, for a variety of
+                organizations, ranging from a 2-person startup running out of a living room, all the
+                way up to companies which are market leaders in their field, with thousands of
+                employees. I have worked across industries such as online publishing, IoT, ad-tech,
+                e-commerce, observability etc. I have built web frontends, backends, and native
+                apps. Lately I have been focusing mostly on web frontends.
+            </p>
+            <p>Feel free to reach out / follow me at one of the platforms below.</p>
         </div>
     );
 }
