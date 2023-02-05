164d1287fb312fcedf643b7d590d5f1620c1206d
Bhashkar Sharma <bhashkarsharma@gmail.com>
2023-02-05 17:01:10 +0100

    Created custom themes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,34 @@
 /* eslint-disable global-require */
 module.exports = {
     content: ['./app/**/*.{ts,tsx,jsx,js}'],
-    theme: {
-        extend: {},
-    },
-    variants: {},
     plugins: [require('daisyui'), require('@tailwindcss/typography')],
     daisyui: {
-        themes: ['emerald', 'dark', 'night'],
-        darkTheme: 'night',
+        themes: [
+            {
+                bee: {
+                    primary: '#f59e0b',
+                    secondary: '#f3f4f6',
+                    accent: '#d97706',
+                    neutral: '#111827',
+                    'base-100': '#facc15',
+                    info: '#06b6d4',
+                    success: '#65a30d',
+                    warning: '#f97316',
+                    error: '#dc2626',
+                },
+                darkbee: {
+                    primary: '#eab308',
+                    secondary: '#ca8a04',
+                    accent: '#fef9c3',
+                    neutral: '#fde047',
+                    'base-100': '#111827',
+                    info: '#3ABFF8',
+                    success: '#36D399',
+                    warning: '#FBBD23',
+                    error: '#F87272',
+                },
+            },
+        ],
+        darkTheme: 'darkbee',
     },
 };
